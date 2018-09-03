974eb166cf7e20338553e17b604304f9c357e7b9
lirenhao <renhao.li@bjyada.com>
2018-09-03 17:53:11 +0800

    QrCode create failed

--- src/views/setAmt.vue
+++ src/views/setAmt.vue
@@ -5,7 +5,8 @@
     </x-header>
     <group class="money-title" :title="$t('Payment amount')"></group>
     <div class="money-box">
-      <x-input class="money-input" v-model="amt" :placeholder="$t('Please enter the payment amount')">
+      <x-input class="money-input" v-model="amt" type="number" step="0.01"
+        :placeholder="$t('Please enter the payment amount')" :is-type="validAmount">
         <div class="money" slot="label">￥</div>
       </x-input>
      </div>
@@ -22,6 +23,9 @@
   Generate QR code:
     en: Generate QR code
     zh-CN: 生成二维码
+  Amount input error: 
+    en: Amount input error
+    zh-CN: 金额输入错误
 </i18n>
 <script>
 import {XHeader, Group, XInput, XButton} from 'vux';
@@ -49,62 +53,87 @@ export default {
     };
   },
   methods: {
+    validAmount(amt) {
+      const regex = /^[1-9]\d*(((,\d{3}){1})?(\.\d{0,2})?)$/;
+      if (regex.test(amt)) {
+        return {valid: true}
+      }
+      return {valid: false, msg: this.$t('Amount input error')}
+    },
     goCreateCode() {
-      api.qrCodeCreate(this.amt).then(data => {
-        if(data){
-          if(data.respCode && data.respCode === '00'){
-            this.$router.replace({name: 'createCode', params: data});
+      if (this.amt !== '' && this.validAmount(this.amt).valid) {
+        api.qrCodeCreate(this.amt).then(data => {
+          if (data) {
+            if (data.respCode && data.respCode === '00') {
+              this.$router.replace({
+                name: 'createCode',
+                params: {...data, timeout: parseInt(data.timeout)},
+              });
+            } else {
+              this.$vux.toast.show({
+                type: 'warn',
+                position: 'default',
+                text:  this.$t('QrCode create failed')
+              })
+            }
           } else {
-
+            this.$vux.toast.show({
+                type: 'warn',
+                position: 'default',
+                text:  this.$t('QrCode create failed')
+              })
           }
-        } else {
-
-        }
-      });
+        })
+      } else {
+        this.$vux.toast.show({
+          type: 'warn',
+          position: 'default',
+          text:  this.$t('Amount input error')
+        })
+      }
     },
   },
 };
 </script>
 
 <style scoped>
-  .money-box {
-    width: 80%;
-    height: 60px;
-    margin-left: 10%;
-    border-bottom: 1px solid #d9d9d9;
-  }
-  .weui-cell {
-    padding-left: 0 !important;
-    padding-bottom: 0 !important;
-  }
+.money-box {
+  width: 80%;
+  height: 60px;
+  margin-left: 10%;
+  border-bottom: 1px solid #d9d9d9;
+}
+.weui-cell {
+  padding-left: 0 !important;
+  padding-bottom: 0 !important;
+}
 
-  .money-title {
-    margin-left: 30px;
-    margin-top: 50px;
-  }
+.money-title {
+  margin-left: 30px;
+  margin-top: 50px;
+}
 
-  .general-btn {
-      width: 66%;
-      height: 38px;
-      margin-left: 17%;
-      background: #67A2F9;
-      color: #fff;
-      border: none;
-      font-size: 15px;
-      margin-top: 96px;
-      border-radius: 4px;
-      text-align: center;
-  }
+.general-btn {
+  width: 66%;
+  height: 38px;
+  margin-left: 17%;
+  background: #67a2f9;
+  color: #fff;
+  border: none;
+  font-size: 15px;
+  margin-top: 96px;
+  border-radius: 4px;
+  text-align: center;
+}
 
-  .weui-btn:after {
-    border: none !important;
-  }
-  .money{
-    font-size: 38px;
-    margin-bottom: 12px;
-  }
-  .money-input {
-    font-size: 14px;
-  }
-
+.weui-btn:after {
+  border: none !important;
+}
+.money {
+  font-size: 38px;
+  margin-bottom: 12px;
+}
+.money-input {
+  font-size: 14px;
+}
 </style>
