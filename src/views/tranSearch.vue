c79e3026888dcd4627e17c62437a3bd9f4bc9bfa
snow <571638661@qq.com>
2018-09-07 13:21:38 +0800

    筛选条件 今日 选中标注

--- src/views/tranSearch.vue
+++ src/views/tranSearch.vue
@@ -8,6 +8,12 @@
       <inline-calendar v-model="tranDate" disable-future>
         <div solt="each-day">afda</div>
       </inline-calendar>
+      <div class="date-today">
+        <span class="blue"></span> 今天
+      </div>
+      <div class="date-selected">
+        <span class="yellow"></span> 选中
+      </div>
 
       <group-title>{{$t('Sub merchant')}}</group-title>
       <picker :data="[merList]" v-model="merNos" ></picker>
@@ -103,14 +109,6 @@ export default {
     color: #5799F9;
   }
 
-  .search-page .inline-calendar td.is-today {
-    /*width: 16px;*/
-    /*height: 16px;*/
-    /*border-radius: 50%;*/
-    /*color: #fff;*/
-    /*background: #5799F9;*/
-  }
-
   /*商户标题*/
   .search-page .weui-cells__title {
     position: absolute;
@@ -123,5 +121,48 @@ export default {
     width: 100%;
     position: absolute !important;
     bottom: 0 !important;
+  }
+
+  /*日历今日样式添加*/
+  .search-page .inline-calendar td.is-today > span.vux-calendar-each-date {
+    background-color: #5799F9;
+    color: #fff;
+  }
+
+  /*今日 选中样式添加*/
+  .date-bottom {
+    width: 100%;
+    height: 40px;
+    background: #fff;
+  }
+
+  .date-today, .date-selected {
+    width: 46%;
+    height: 32px;
+    float: left;
+    font-size: 12px;
+    color: #999;
+    line-height: 32px;
+    background: #fff;
+    text-align: right;
+    padding-right: 4%;
+  }
+
+  .date-selected {
+    text-align: left !important;
+    padding-left: 4% !important;
+    padding-right: 0 !important;
+  }
+
+  .date-today span, .date-selected span {
+    display: inline-block;
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    background: #FF9900;
+  }
+
+  .blue {
+    background: #5799F9 !important;
   }
 </style>
