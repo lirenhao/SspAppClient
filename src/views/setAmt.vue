7e3b7ead8c2c0f163a10405dad4683bc0816d65a
lirenhao <renhao.li@bjyada.com>
2018-11-30 10:50:48 +0800

    设置金额格式修改

--- src/views/setAmt.vue
+++ src/views/setAmt.vue
@@ -1,48 +1,77 @@
 <template>
   <div>
-    <x-header :title="$t(title)" :left-options="{showBack}">
-    </x-header>
+    <x-header :title="$t(title)" :left-options="{showBack}"></x-header>
+    <div class="Payment-options">
+      <group class="money-title" :title="$t('Please choose the payment channel')"></group>
+      <br>
+      <checker v-model="channel" :radio-required="true" class="weui-grids"
+        default-item-class="channel-item" selected-item-class="channel-item-selected">
+        <checker-item value="01" class="weui-grid">
+          <img class="weui-grid__icon" src="../assets/UnionPay.png">
+          <span class="weui-grid__label">{{$t('UnionPay')}}</span>
+        </checker-item>
+        <checker-item value="02" class="weui-grid">
+          <img class="weui-grid__icon" src="../assets/WeChat.png">
+          <span class="weui-grid__label">{{$t('WeChat')}}</span>
+        </checker-item>
+        <checker-item value="03" class="weui-grid">
+          <img class="weui-grid__icon" src="../assets/Alipay.png">
+          <span class="weui-grid__label">{{$t('Alipay')}}</span>
+        </checker-item>
+      </checker>
+    </div>
     <div class="BG">
       <group class="money-title" :title="$t('Please enter the payment amount')"></group>
       <div class="money-box">
-        <label class="money" >{{ccyType.ccySymbol}}</label>
-        <input class="money-input" v-model="amt" type="number" step="0.01" :placeholder="ccyType.ccyEname"/>
+        <label class="money">{{ccyType.ccySymbol}}</label>
+        <input
+          class="money-input"
+          v-model="amt"
+          type="number"
+          step="0.01"
+          :placeholder="ccyType.ccyEname"
+        >
       </div>
     </div>
-    <div class="Payment-options">
-      <group class="money-title" :title="$t('Please choose the payment channel')"></group>
-      <group>
-        <radio :options="options" v-model="channel">
-          <div slot-scope="props" slot="each-item">
-            <img width="26" class="UnionPay" src="../assets/UnionPay.png" v-if="props.index === 0"/>
-            <img width="24" class="WeChat" src="../assets/WeChat.png" v-if="props.index === 1"/>
-            <img width="24" class="WeChat" src="../assets/Alipay.png" v-if="props.index === 2"/>
-           {{props.label}}
-          </div>
-        </radio>
-      </group>
-    </div>
-    <x-button class="general-btn" type="primary" @click.native="goCreateCode">{{$t('Generate QR code')}}</x-button>
+    <x-button
+      class="general-btn"
+      type="primary"
+      @click.native="goCreateCode"
+    >{{$t('Generate QR code')}}</x-button>
   </div>
 </template>
 <script>
-import {XHeader, Group, XInput, XButton, Radio} from 'vux';
-import api from '../api';
+import {
+  XHeader,
+  Group,
+  Grid,
+  GridItem,
+  XInput,
+  XButton,
+  Radio,
+  Checker,
+  CheckerItem
+} from "vux";
+import api from "../api";
 
 export default {
-  name: 'setAmt',
+  name: "setAmt",
   components: {
     XHeader,
     Group,
+    Grid,
+    GridItem,
     XInput,
     XButton,
     Radio,
+    Checker,
+    CheckerItem
   },
   created: function() {
     if (!window.localStorage.token) {
-      this.$router.push({name: 'login', params: {isClear: false}});
+      this.$router.push({ name: "login", params: { isClear: false } });
     } else {
-      api.userInfo().then(data => this.ccyType = data.ccyType)
+      api.userInfo().then(data => (this.ccyType = data.ccyType));
     }
   },
   props: {
@@ -53,146 +82,147 @@ export default {
       isShowNav: this.$route.meta.isShowNav,
       title: this.$route.meta.title,
       showBack: this.$route.meta.showBack,
-      amt: this.tranAmt ? this.tranAmt : '',
+      amt: this.tranAmt ? this.tranAmt : "",
       ccyType: {},
-      channel: '01',
+      channel: "01",
       options: [
         {
-          key: '01',
-          value: this.$t('UnionPay')
-        }, {
-          key: '02',
-          value: this.$t('WeChat')
-        }, {
-          key: '03',
-          value: this.$t('Alipay')
+          key: "01",
+          value: this.$t("UnionPay")
+        },
+        {
+          key: "02",
+          value: this.$t("WeChat")
+        },
+        {
+          key: "03",
+          value: this.$t("Alipay")
         }
-      ],
+      ]
     };
   },
   methods: {
     validAmount(amt) {
       const regex = /^[1-9]\d*(((,\d{3}){1})?(\.\d{0,2})?)$/;
       if (regex.test(amt)) {
-        return {valid: true}
+        return { valid: true };
       }
-      return {valid: false, msg: this.$t('Amount input error')}
+      return { valid: false, msg: this.$t("Amount input error") };
     },
     goCreateCode() {
-      if (this.amt !== '' && this.validAmount(this.amt).valid) {
+      if (this.amt !== "" && this.validAmount(this.amt).valid) {
         api.qrCodeCreate(this.amt, this.channel).then(data => {
           if (data) {
-            if (data.respCode && data.respCode === '00') {
+            if (data.respCode && data.respCode === "00") {
               this.$router.replace({
-                name: 'createCode',
-                params: {...data, timeout: parseInt(data.timeout)},
+                name: "createCode",
+                params: { ...data, timeout: parseInt(data.timeout) }
               });
             } else {
               this.$vux.toast.show({
-                type: 'warn',
-                position: 'default',
-                text:  this.$t('QrCode create failed')
-              })
+                type: "warn",
+                position: "default",
+                text: this.$t("QrCode create failed")
+              });
             }
           } else {
             this.$vux.toast.show({
-                type: 'warn',
-                position: 'default',
-                text:  this.$t('QrCode create failed')
-              })
+              type: "warn",
+              position: "default",
+              text: this.$t("QrCode create failed")
+            });
           }
-        })
+        });
       } else {
         this.$vux.toast.show({
-          type: 'warn',
-          position: 'default',
-          text:  this.$t('Amount input error')
-        })
+          type: "warn",
+          position: "default",
+          text: this.$t("Amount input error")
+        });
       }
-    },
-  },
+    }
+  }
 };
 </script>
 
 <style scoped>
-  /*输入金额修改为白色背景*/
-  .BG {
-    background: #fff;
-    margin-top: -0.77em;
-    padding: 30px 0;
-  }
+/*输入金额修改为白色背景*/
+.BG {
+  background: #fff;
+  margin-top: -0.77em;
+  padding: 30px 0;
+}
 
-  /*支付选择*/
-  .Payment-options {
-    background: #fff;
-    margin-top: 16px;
-    padding-top: 12px;
-  }
+/*支付选择*/
+.Payment-options {
+  background: #fff;
+  margin-top: 16px;
+  padding-top: 12px;
+}
 
-  .Payment-options .money-title {
-    margin-bottom: -10px;
-  }
+.Payment-options .money-title {
+  margin-bottom: -10px;
+}
 
-  /*标题*/
-  .money-title {
-    margin-left: 10px;
-  }
+/*标题*/
+.money-title {
+  margin-left: 10px;
+}
 
-  /*金额图标*/
-  .money {
-    font-size: 38px;
-    margin-bottom: 12px;
-  }
+/*金额图标*/
+.money {
+  font-size: 38px;
+  margin-bottom: 12px;
+}
 
-  /*输入金额框*/
-  .money-box {
-    width: 84%;
-    height: 60px;
-    margin-left: 8%;
-    border-bottom: 1px solid #d9d9d9;
-  }
+/*输入金额框*/
+.money-box {
+  width: 84%;
+  height: 60px;
+  margin-left: 8%;
+  border-bottom: 1px solid #d9d9d9;
+}
 
-  /*输入框*/
-  .money-input {
-    width: 84%;
-    height: 38px;
-    background: none;
-    border: none;
-    outline:medium;
-    font-size: 38px;
-  }
+/*输入框*/
+.money-input {
+  width: 84%;
+  height: 38px;
+  background: none;
+  border: none;
+  outline: medium;
+  font-size: 38px;
+}
 
-  /*间距调整*/
-  .weui-cell {
-    padding-left: 0 !important;
-    padding-bottom: 0 !important;
-  }
+/*间距调整*/
+.weui-cell {
+  padding-left: 0 !important;
+  padding-bottom: 0 !important;
+}
 
-  /*按钮*/
-  .general-btn {
-    width: 66%;
-    height: 38px;
-    margin-left: 17%;
-    color: #fff;
-    border: none;
-    font-size: 15px;
-    margin-top: 96px;
-    border-radius: 4px;
-    text-align: center;
-  }
+/*按钮*/
+.general-btn {
+  width: 66%;
+  height: 38px;
+  margin-left: 17%;
+  color: #fff;
+  border: none;
+  font-size: 15px;
+  margin-top: 96px;
+  border-radius: 4px;
+  text-align: center;
+}
 
-  /*删除按钮边框*/
-  .weui-btn:after {
-    border: none !important;
-  }
+/*删除按钮边框*/
+.weui-btn:after {
+  border: none !important;
+}
 
-  .WeChat {
-    margin-bottom: -4px;
-    margin-right: 4px;
-  }
-  .UnionPay {
-    margin-bottom: -2px;
-    margin-right: 2px;
-  }
-
+.channel-item {
+  text-align: center;
+  background-color: #fff;
+}
+.channel-item-selected {
+  background: #b81c22;
+  border-color: #ff4a00;
+}
 </style>
